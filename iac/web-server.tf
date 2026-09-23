f5314acb730ef1f002f26e0d1c884fecfb049ff5
Gordillo Saona Diego Manuel <dsgordillo21@gmail.com>
2026-09-23 08:50:34 -0500

    feat: add frontend content for dev and qa

--- iac/web-server.tf
+++ iac/web-server.tf
@@ -11,6 +11,23 @@ resource "docker_container" "web_dev" {
     internal = 80
     external = var.web_server_port["dev"]
   }
+
+  upload {
+    content = <<-EOT
+      <!DOCTYPE html>
+      <html lang="es">
+      <head>
+        <meta charset="UTF-8">
+        <title>DEV</title>
+      </head>
+      <body>
+        <h1>Frontend DEV</h1>
+      </body>
+      </html>
+    EOT
+
+    file = "/usr/share/nginx/html/index.html"
+  }
 }
 
 resource "docker_container" "web_qa" {
@@ -20,5 +37,22 @@ resource "docker_container" "web_qa" {
   ports {
     internal = 80
     external = var.web_server_port["qa"]
+  }
+
+  upload {
+    content = <<-EOT
+      <!DOCTYPE html>
+      <html lang="es">
+      <head>
+        <meta charset="UTF-8">
+        <title>QA</title>
+      </head>
+      <body>
+        <h1>Frontend QA</h1>
+      </body>
+      </html>
+    EOT
+
+    file = "/usr/share/nginx/html/index.html"
   }
 }
